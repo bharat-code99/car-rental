45a6400a110cd2f8b32ace1c4083f71c82a2ada6
bharat-code99 <2491bhr@gmail.com>
2026-05-14 23:24:39 +0530

    Completed Owner Dashboard Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { Suspense, lazy, useState } from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router";
-import Home from "./pages/Home";
-import CarDetails from "./pages/CarDetails";
-import Cars from "./pages/Cars";
-import MyBookings from "./pages/MyBookings";
 import Footer from "./components/Footer";
-import Layout from "./pages/owner/Layout";
-import Dashboard from "./pages/owner/Dashboard";
-import ManageCars from "./pages/owner/ManageCars";
-import ManageBookings from "./pages/owner/ManageBookings";
-import AddCar from "./pages/owner/AddCar";
+import Loader from "./components/Loader";
+
+const Home = lazy(() => import("./pages/Home"));
+const CarDetails = lazy(() => import("./pages/CarDetails"));
+const Cars = lazy(() => import("./pages/Cars"));
+const MyBookings = lazy(() => import("./pages/MyBookings"));
+const Layout = lazy(() => import("./pages/owner/Layout"));
+const Dashboard = lazy(() => import("./pages/owner/Dashboard"));
+const ManageCars = lazy(() => import("./pages/owner/ManageCars"));
+const ManageBookings = lazy(() => import("./pages/owner/ManageBookings"));
+const AddCar = lazy(() => import("./pages/owner/AddCar"));
 
 export default function App() {
   const [_, setShowLogin] = useState(false);
@@ -20,18 +22,20 @@ export default function App() {
     <>
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/car-details/:id" element={<CarDetails />} />
-        <Route path="/cars" element={<Cars />} />
-        <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/owner" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="add-car" element={<AddCar />} />
-          <Route path="manage-cars" element={<ManageCars />} />
-          <Route path="manage-bookings" element={<ManageBookings />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/car-details/:id" element={<CarDetails />} />
+          <Route path="/cars" element={<Cars />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="add-car" element={<AddCar />} />
+            <Route path="manage-cars" element={<ManageCars />} />
+            <Route path="manage-bookings" element={<ManageBookings />} />
+          </Route>
+        </Routes>
+      </Suspense>
 
       {!isOwnerPath && <Footer />}
     </>
